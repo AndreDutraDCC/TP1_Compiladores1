5e7f224ee46f37ec611fe38637e877c8be6f72f3
Pedro Dias <pedrodias504@gmail.com>
2022-02-10 21:08:34 -0300

    Corrigindo recursão infinita no BINOP

--- Intermediate/Nodes/BINOP.java
+++ Intermediate/Nodes/BINOP.java
@@ -7,8 +7,7 @@ import java.util.ArrayList;
 public class BINOP extends Stm {
     public BINOP(int o, Stm e1, Stm e2) {
         name_ = "BINOP";
-        children = new ArrayList<Stm>(3);
-        children.add(this);
+        children = new ArrayList<Stm>(2);
         children.add(e1);
         children.add(e2);
         op_ = o;
@@ -16,7 +15,7 @@ public class BINOP extends Stm {
 
     @Override
     public String stringRepresentation(String prefix) {
-        return prefix + name_ + "(" + operatorName() + ",\n" + childrenToString(" " + prefix) + ")";
+        return prefix + name_ + "(" + operatorName() + ",\n" + childrenToString(prefix) + ")";
     }
 
     private String operatorName() {
